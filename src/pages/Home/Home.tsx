60171e91732eebd22c42024613955d3946a28ebc
unknown <yoyoanusingh27@gmail.com>
2020-08-12 23:33:18 +0530

    level screen first

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import { IonPage } from '@ionic/react';
+import { IonPage, IonContent } from '@ionic/react';
 import React from 'react';
-import './Home.css';
+import styles from './Home.module.css';
 import AppBar from '../../components/AppBar/AppBar';
 import SideBar from '../../components/SideBar/SideBar';
 
@@ -9,8 +9,32 @@ const Home: React.FC = () => {
     <IonPage>
       <AppBar />
       <SideBar title="Teachable Machine" />
+      <IonContent className={styles.bg} style={{margin: 'auto'}}>
+        <Container />
+      </IonContent>
     </IonPage>
   );
 };
+
+function Container() {
+  return (
+    <div className={styles.container}>
+      <BubbleCircle text='Beginner' />
+      <BubbleCircle text='Intermediate' />
+    </div>
+  )
+}
+
+function BubbleCircle(props: any) {
+  return (
+    <div className={styles.bubbleCon}>
+      <div className={styles.levelInfo}>
+        <p style={{width: 'fit-content' ,margin: 'auto'}}>[img]</p>
+        <p>{props.text}</p>
+      </div>
+    </div>
+  )
+}
+
 
 export default Home;
